50f798c998d72e694ecf30b4ca3aa7c145d2b748
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-09-19 12:12:01 +0000

    Mon, Sep 19, 2022, 12:12 PM +00:00

---
+++ indoor/indoor.py
@@ -0,0 +1,6 @@
+# Ask student for their college major
+major = input("What's your college major? ").strip()
+
+# Wish the student a good luck
+print(f"All the best with your major, {major}".lower())
+
