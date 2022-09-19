17b7d68c2bbfafa6b5d988e0f2026b7d62b88ccb
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-09-19 15:33:13 +0000

    Mon, Sep 19, 2022, 3:33 PM +00:00

---
+++ playback/playback.py
@@ -0,0 +1,8 @@
+
+name = input("What's your name? ").strip().title()
+
+# Split user's name into first and last name
+first, last = name.split(" ")
+
+# Say hello to user
+print(f"hello, {name}", sep= "..." )
