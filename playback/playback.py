9ee0102b0655246e99d796ca198114f210ea916a
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-09-21 10:20:48 +0000

    Wed, Sep 21, 2022, 10:20 AM +00:00

--- playback/playback.py
+++ playback/playback.py
@@ -3,11 +3,5 @@ name = input("What's your name? ").strip()
 
 
 # Say hello to user
-print(f"hello {name}" )
+print(f"hello {name}" .expandtabs())
 
-#Space between the output
-# default tabsize is 8
-name = 'hello/name'
-result = name.expandtabs()
-
-print(result)
