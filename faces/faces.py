e96f23ef87762fd6c0e90c0abfbf35c903b57353
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-09-28 10:32:03 +0000

    Wed, Sep 28, 2022, 10:32 AM +00:00

---
+++ faces/faces.py
@@ -0,0 +1,19 @@
+def main():
+        #Get input from user
+    user = input()
+
+
+        # Call convert function
+
+
+        # print output
+
+def convert(..)
+    #Replace :) for happy emoji
+
+
+    # Replace :( for sad emoji
+
+    # Return string
+
+main()
