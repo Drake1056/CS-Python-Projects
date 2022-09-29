2de00f8436722f54ab6be5fc11310c4419411acb
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-09-29 10:26:14 +0000

    Thu, Sep 29, 2022, 10:26 AM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -6,7 +6,7 @@ def main():
 
 def dollars_to_float(d):
    d_without_dollar_sign = d.replace("$", "")
-    return float(d_without_dollar_sign)
+   return float(d_without_dollar_sign)
 
 
 def percent_to_float(p):
