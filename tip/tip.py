86787b2915d079d41709b19f3b6ccac95766d77d
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-09-29 09:59:05 +0000

    Thu, Sep 29, 2022, 9:59 AM +00:00

---
+++ tip/tip.py
@@ -0,0 +1,12 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal?" ))
+    percent = (input("What percent would you like to tip?" ))
+    tip = dollars * percent
+    print(f"Leave ${tip: 2f}")
+
+def dollars_to_float(d):
+
+
+
+
+def percent_to_float(p):
