38b99e8d2b28e81471a56783bab19ebc895d47be
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-08 11:13:49 +0000

    Sat, Oct 8, 2022, 11:13 AM +00:00

--- parity.py
+++ parity.py
@@ -1,6 +1,15 @@
 def main():
 
-X = int(input("What's X? "))
+    X = int(input("What's X? "))
+    if is_even(X):
+        print("Even")
+    else:
+        print("Odd)
+
+def is_even(n)
+    if n % 2 == 0:
+        
 
 
+main()
 
