45da2912d611e5ee8434e8ce966c69be47085bdb
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-08 09:57:58 +0000

    Sat, Oct 8, 2022, 9:57 AM +00:00

--- compare.py
+++ compare.py
@@ -1,6 +1,11 @@
-x = int(input("What's X" ))
-y = int(input("What's Y" ))
+x = int(input("What's X? " ))
+y = int(input("What's Y? " ))
 
 if X < Y:
     print("X is less than Y")
-    
+
+if X > Y:
+    print("X is Greater than Y")
+
+if X == Y:
+    print("X is equal to Y")
