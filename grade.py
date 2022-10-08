1943e549293b432f432592ca49af3cc92aa5a373
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-08 10:40:05 +0000

    Sat, Oct 8, 2022, 10:40 AM +00:00

--- grade.py
+++ grade.py
@@ -1,15 +1,15 @@
 Score = int(input("Score: "))
 
-if 90<= Score  <= 100:
+if Score >= 90
     print("Grade: A")
 
-elif 80<= Score  < 90:
+elif Score >= 80
     print("Grade: B")
 
-elif 70<= Score  <80:
+elif Score >= 70
         print("Grade: C")
 
-elif 60<= Score  < 70:
+elif Score >= 60
     print("Grade: D")
 
 else:
