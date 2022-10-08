cf471142b0da64fe7dc8eb9542642907d3d0b983
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-08 10:22:29 +0000

    Sat, Oct 8, 2022, 10:22 AM +00:00

--- compare1.py
+++ compare1.py
@@ -1,8 +1,8 @@
 x = int(input("What's X? " ))
 y = int(input("What's Y? " ))
 
-if x != y:
-    print("X is not equal to Y" )
+if x == y:
+    print("X is equal to Y" )
 
 else:
-    print("X is equal to Y" )
+    print("X is not equal to Y" )
