ec1a56b876752f581234f9de773d6db41ac4c2a5
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-08 11:30:43 +0000

    Sat, Oct 8, 2022, 11:30 AM +00:00

--- house.py
+++ house.py
@@ -3,4 +3,11 @@ name = input("What's your name?" )
 if name == "Harry":
     print("Gryffindor")
 elif name == "Hermione":
-    print
+    print("Gryffindor")
+elif name == "Ron":
+    ("Gryffindor")
+elif name == "Draco":
+    print("Syltherin")
+
+else:
+    print("Who?")
