fbeba91d623a00f6cc464def34d3b04db12efeda
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-10 16:48:23 +0000

    Mon, Oct 10, 2022, 4:48 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,7 +3,7 @@ filename: input("File name: ")
 new_filename = filename.lower().strip()
 
 # gif or jpeg or jpg or png, print "image/type"
-if 'gif' in filename:
+if 'gif' | 'jpeg' | 'png' | 'jpg'  in filename:
     print("image/type")
 
 # If PDF of Zip, print "application/PDF"
