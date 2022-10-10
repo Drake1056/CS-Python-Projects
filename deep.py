44de4e0c100b816b9cf90c2d8defcc6d30921f5d
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-10 11:16:40 +0000

    Mon, Oct 10, 2022, 11:16 AM +00:00

--- deep.py
+++ deep.py
@@ -1,8 +1,9 @@
 # 42 forty-two forty two
 
-answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?" ).lower()
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?" ).lower().strip()
 
 if answer == "42" or answer == "forty-two" or answer == "forty two":
     print("Yes")
 else:
     print("No")
+
