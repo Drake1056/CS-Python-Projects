7cb307a98f9023d14868d3681a2fcf937dde19a0
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-10 16:59:24 +0000

    Mon, Oct 10, 2022, 4:59 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,18 +5,24 @@ new_filename = filename.lower()
 # gif or jpeg or jpg or png, print "image/type"
 if '.gif'  in new_filename:
     print("image/type")
-if 'jpeg' in new_filename:
+if '.jpeg' in new_filename:
     print("image/jpeg")
-if 'jpg' in new_filename:
+if '.jpg' in new_filename:
     print("image/jpg")
-if 'png' in  new_filename:
+elif '.png' in  new_filename:
     print("image/png")
 
-# If PDF of Zip, print "application/PDF"
-
+# If PDF of Zip, print "application/type"
+if '.pdf' in new_filename:
+    print("application/pdf")
+elif '.Zip' in new_filename:
+    print("application/Zip")
 
 # If txt, print "text/plain"
-
+if '.txt' in new_filename:
+    print("text/plain)
 
 
 # otherwise, print "application/octet-stream"
+else:
+    print("application/octet-stream")
