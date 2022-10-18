b8db8a89f269ee91fb08b7975cc10b02b44f2b7b
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-18 16:15:05 +0000

    Tue, Oct 18, 2022, 4:15 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -7,15 +7,16 @@ answer = input("What time is it?" )
 time = convert(answer)
 
 # Breakfast between 7:00 and 8:00
-if 
-
+if time >= 7 and time <= 8:
+    print ("breakfast time")
 
 # lunch between 12:00 and 13:00
-
-
+if time >= 12 and time <= 13:
+    print("lunch time")
 
 # dinner between 18:00 and 19:00
-
+if time >= 18 and time <= 19:
+    print("dinner time")
 
 def convert(time):
 
