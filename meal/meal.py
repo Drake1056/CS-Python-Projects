9626eed5e4c597349daf2b1b1c32e6e86d97d8ee
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-18 16:09:57 +0000

    Tue, Oct 18, 2022, 4:09 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -5,7 +5,7 @@ answer = input("What time is it?" )
 
 # Convert Function
 time = convert(answer)
-
+print(time)
 
 # Breakfast between 7:00 and 8:00
 
@@ -23,11 +23,11 @@ def convert(time):
 # Get hour and minute
 hours, minutes = time.split(":")
 
-
 # Convert time to float
-
+new_minute = float(minutes)/ 60
 
 # Return result
+return float(hours) + new_minute
 
 if _name_ == "_main_":
 
