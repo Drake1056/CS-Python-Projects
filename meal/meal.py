16468d759644b4fddd0a10c8c436625f17fbcdb9
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-18 16:20:17 +0000

    Tue, Oct 18, 2022, 4:20 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,33 +1,33 @@
 def main():
 
-# Get time from user
-answer = input("What time is it?" )
+    # Get time from user
+    answer = input("What time is it?" )
 
-# Convert Function
-time = convert(answer)
+    # Convert Function
+    time = convert(answer)
 
-# Breakfast between 7:00 and 8:00
-if time >= 7 and time <= 8:
-    print ("breakfast time")
+    # Breakfast between 7:00 and 8:00
+    if time >= 7 and time <= 8:
+        print ("breakfast time")
 
-# lunch between 12:00 and 13:00
-if time >= 12 and time <= 13:
-    print("lunch time")
+    # lunch between 12:00 and 13:00
+     if time >= 12 and time <= 13:
+        print("lunch time")
 
-# dinner between 18:00 and 19:00
-if time >= 18 and time <= 19:
-    print("dinner time")
+    # dinner between 18:00 and 19:00
+    if time >= 18 and time <= 19:
+        print("dinner time")
 
 def convert(time):
 
-# Get hour and minute
-hours, minutes = time.split(":")
+    # Get hour and minute
+     hours, minutes = time.split(":")
 
-# Convert time to float
-new_minute = float(minutes)/ 60
+    # Convert time to float
+    new_minute = float(minutes)/ 60
 
-# Return result
-return float(hours) + new_minute
+    # Return result
+        return float(hours) + new_minute
 
 if _name_ == "_main_":
 
