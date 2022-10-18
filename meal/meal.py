2d2b11c7ade9f2d1867f8ac879b426ae7e6dadf7
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2022-10-18 15:55:38 +0000

    Tue, Oct 18, 2022, 3:55 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,5 +1,21 @@
 def main():
 
+# Get time from user
+
+
+# Convert Function
+
+
+
+# Breakfast between 7:00 and 8:00
+
+
+
+# lunch between 12:00 and 13:00
+
+
+
+# dinner between 18:00 and 19:00
 
 
 def convert(time):
@@ -8,4 +24,3 @@ def convert(time):
 
 if _name_ == "_main_":
 
-    
