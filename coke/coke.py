8d3d24fe275df253c1b889cd00b68adf3b6e2037
Drake1056 <111249270+Drake1056@users.noreply.github.com>
2023-05-16 06:04:49 +0000

    Tue, May 16, 2023, 6:04 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,4 +4,5 @@ while amount_due > 0:
     print("Amount Due: ", amount_due)
     coin = int(input("Insert Coin: "))
 
-    
+    if coin == 25 or coin == 10 or coin == 5:
+        amount_due -= coin
